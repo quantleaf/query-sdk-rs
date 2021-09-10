21d844abaa780d0a31e2f0c3018286d258786dca
marcus-pousette <marcus.pousette@quantleaf.com>
2021-09-10 21:27:58 +0200

    Serialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,129 @@
-pub enum ConditionElement {
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum Condition {
     ConditionAnd(ConditionAnd),
     ConditionNot(ConditionNot),
     ConditionOr(ConditionOr),
+    ConditionCompare(ConditionCompare),
 }
-
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct QuerySchema {
     pub from: Vec<String>,
-    pub condition: ConditionElement,
+    pub condition: Condition,
 }
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Unknown {
     pub offset: usize,
     pub length: usize,
 }
-
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct QueryResult {
     pub query: Option<Vec<QuerySchema>>,
     pub unknown: Option<Vec<Unknown>>,
 }
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 
 pub struct ConditionCompare {
     pub compare: Compare,
 }
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
 pub struct ConditionAnd {
-    pub and: Vec<ConditionElement>,
-}
-pub struct ConditionNot {
-    pub not: Box<ConditionElement>,
-}
-pub struct ConditionOr {
-    pub or: Vec<ConditionElement>,
+    pub and: Vec<Condition>,
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct ConditionNot {
+    pub not: Box<Condition>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct ConditionOr {
+    pub or: Vec<Condition>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum CompareValue {
     String(String),
     F64(f64),
     Bool(bool),
 }
-pub struct Compare {
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum Compare {
+    CompareEq(CompareEq),
+    CompareLt(CompareLt),
+    CompareLte(CompareLte),
+    CompareGt(CompareGt),
+    CompareGte(CompareGte),
+    CompareNot(CompareNot),
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+pub struct CompareEq {
     pub key: String,
-    pub lt: Option<CompareValue>,
-    pub lte: Option<CompareValue>,
-    pub gt: Option<CompareValue>,
-    pub gte: Option<CompareValue>,
-    pub eq: Option<CompareValue>,
-    pub neq: Option<CompareValue>,
+    pub eq: CompareValue,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+pub struct CompareLt {
+    pub key: String,
+    pub lt: CompareValue,
+}
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct CompareLte {
+    pub key: String,
+    pub lte: CompareValue,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+pub struct CompareGt {
+    pub key: String,
+    pub gt: CompareValue,
+}
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+pub struct CompareGte {
+    pub key: String,
+    pub gte: CompareValue,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+pub struct CompareNot {
+    pub key: String,
+    pub not: CompareValue,
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn serialization_deserialization() {
+        let query = QueryResult {
+            query: Some(vec![QuerySchema {
+                from: vec!["from".to_string()],
+                condition: Condition::ConditionAnd(ConditionAnd {
+                    and: vec![Condition::ConditionOr(ConditionOr {
+                        or: vec![Condition::ConditionCompare(ConditionCompare {
+                            compare: Compare::CompareEq(CompareEq {
+                                key: "key".to_string(),
+                                eq: CompareValue::String("eq".to_string()),
+                            }),
+                        })],
+                    })],
+                }),
+            }]),
+            unknown: None,
+        };
+        let json = serde_json::to_string(&query).unwrap();
+        let query_from_json = serde_json::from_str::<QueryResult>(json.as_str()).unwrap();
+        assert_eq!(query, query_from_json);
     }
 }
