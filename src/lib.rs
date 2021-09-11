16ad774a48802526a9221471ab6789dfe3e6c721
marcus-pousette <marcus.pousette@quantleaf.com>
2021-09-11 08:21:04 +0200

    Clone trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,59 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum Condition {
     ConditionAnd(ConditionAnd),
     ConditionNot(ConditionNot),
     ConditionOr(ConditionOr),
     ConditionCompare(ConditionCompare),
 }
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct QuerySchema {
     pub from: Vec<String>,
     pub condition: Condition,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct Unknown {
     pub offset: usize,
     pub length: usize,
 }
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct QueryResult {
     pub query: Option<Vec<QuerySchema>>,
     pub unknown: Option<Vec<Unknown>>,
 }
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct ConditionCompare {
     pub compare: Compare,
 }
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct ConditionAnd {
     pub and: Vec<Condition>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct ConditionNot {
     pub not: Box<Condition>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct ConditionOr {
     pub or: Vec<Condition>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum CompareValue {
     String(String),
     F64(f64),
     Bool(bool),
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum Compare {
     CompareEq(CompareEq),
     CompareLt(CompareLt),
@@ -61,39 +63,39 @@ pub enum Compare {
     CompareNot(CompareNot),
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct CompareEq {
     pub key: String,
     pub eq: CompareValue,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct CompareLt {
     pub key: String,
     pub lt: CompareValue,
 }
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct CompareLte {
     pub key: String,
     pub lte: CompareValue,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct CompareGt {
     pub key: String,
     pub gt: CompareValue,
 }
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct CompareGte {
     pub key: String,
     pub gte: CompareValue,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
 pub struct CompareNot {
     pub key: String,
